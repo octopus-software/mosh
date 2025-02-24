01e75a9157f6af3716504edb69552c7a63b25ce6
taiga <taigabox224@gmail.com>
2025-02-24 19:14:47 +0900

    docker関連の環境ファイルの処理

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     base: '',
     server: {
-        https: true, // 開発サーバーがHTTPSを使用するように設定
+        // https: true, // 開発サーバーがHTTPSを使用するように設定
     },
     plugins: [
         laravel({
